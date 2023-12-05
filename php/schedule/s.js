b3d66118808cbf10077ad77b919dfeec26e23ed9
Yonatan Girmachew <yonatangirmachew3@gmail.com>
2023-12-04 16:50:16 -0800

    Schedule for the user page

---
+++ php/schedule/s.js
@@ -0,0 +1,25 @@
+function showDetailsA(movie) {
+    document.getElementById("movieTitleA").innerText = movie;
+    document.getElementById("modalA").style.display = "flex";
+  }
+  function showDetailsB(movie) {
+    document.getElementById("movieTitle").innerText = movie;
+    document.getElementById("modal").style.display = "flex";
+  }
+  function showDetailsC(movie) {
+    document.getElementById("movieTitle").innerText = movie;
+    document.getElementById("modal").style.display = "flex";
+  }
+  function showDetailsD(movie) {
+    document.getElementById("movieTitle").innerText = movie;
+    document.getElementById("modal").style.display = "flex";
+  }
+  function showDetailsE(movie) {
+    document.getElementById("movieTitle").innerText = movie;
+    document.getElementById("modal").style.display = "flex";
+  }
+
+  function closeModal() {
+    document.getElementById("modalA").style.display = "none";
+    document.getElementById("modal").style.display = "none";
+  }
