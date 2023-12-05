8e01c12b8e0483bac833ab11f542461b5261d4c8
Yonatan Girmachew <yonatangirmachew3@gmail.com>
2023-12-05 15:42:44 -0800

    Update ownerPage.php

--- php/ownerPage/ownerPage.php
+++ php/ownerPage/ownerPage.php
@@ -5,12 +5,12 @@
   <meta charset="UTF-8" />
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
   <title>Owner Page</title>
-  <link rel="stylesheet" href="../../public/css/ownerPage/owner.css?v=<?echo time()?>" />
+  <link rel="stylesheet" href="../../public/css/ownerPage/owner.css?v=<? echo time() ?>" />
 </head>
 
 <body>
   <header>
-    <img src="owner.jpg" alt="Owner Logo" class="logo" />
+    <img src="/public/img/owner.jpg" alt="Owner Logo" class="logo" />
     <div class="name">
       <p>Alem Cinema</p>
     </div>
@@ -19,40 +19,40 @@
   </header>
   <section class="report-section">
     <div class="menu" id="menu">
-            <div class="menuCon">
-                <div>
-                    <img src="../../public/img/exit.svg?v=<? echo time() ?>" id="exit" alt="">
-                </div>
-                <div>
-                    <h2>ALEM CINEMA</h2>
-                    <hr>
-                    <a href="http://localhost/movie/php/userPage/">
-                        <p>Home</p>
-                    </a>
-                    <hr>
-                    <a href="http://localhost/movie/php/menu/blog.php">
-                        <p>About</p>
-                    </a>
-                    <hr>
-                    <a href="http://localhost/movie/php/menu/contact.php">
-                      <p>Contact</p>
-                    </a>
-                    <hr>
-                    <a href="#">
-                        <p>Logout</p>
-                    </a>
-                    <hr>
-                    <small>CONNECT WITH US</small>
-                    <div>
-                        <a href="#"><img src="../../public/img/menu/instagram.svg?v=<?php echo time() ?>" alt="" srcset=""></a>
-                        <a href="#"><img src="../../public/img/menu/telegram.svg?v=<?php echo time() ?>" alt="" srcset=""></a>
-                        <a href="#"><img src="../../public/img/menu/twitter.svg?v=<?php echo time() ?>" alt="" srcset=""></a>
-                        <a href="#"><img src="../../public/img/menu/whatsapp.svg?v=<?php echo time() ?>" alt="" srcset=""></a>
-                        <a href="#"><img src="../../public/img/menu/facebook.svg?v=<?php echo time() ?>" alt="" srcset=""></a>
-                    </div>
-                </div>
-            </div>
+      <div class="menuCon">
+        <div>
+          <img src="../../public/img/exit.svg?v=<? echo time() ?>" id="exit" alt="">
         </div>
+        <div>
+          <h2>ALEM CINEMA</h2>
+          <hr>
+          <a href="http://localhost/movie/php/userPage/">
+            <p>Home</p>
+          </a>
+          <hr>
+          <a href="http://localhost/movie/php/menu/blog.php">
+            <p>About</p>
+          </a>
+          <hr>
+          <a href="http://localhost/movie/php/menu/contact.php">
+            <p>Contact</p>
+          </a>
+          <hr>
+          <a href="#">
+            <p>Logout</p>
+          </a>
+          <hr>
+          <small>CONNECT WITH US</small>
+          <div>
+            <a href="#"><img src="../../public/img/menu/instagram.svg?v=<?php echo time() ?>" alt="" srcset=""></a>
+            <a href="#"><img src="../../public/img/menu/telegram.svg?v=<?php echo time() ?>" alt="" srcset=""></a>
+            <a href="#"><img src="../../public/img/menu/twitter.svg?v=<?php echo time() ?>" alt="" srcset=""></a>
+            <a href="#"><img src="../../public/img/menu/whatsapp.svg?v=<?php echo time() ?>" alt="" srcset=""></a>
+            <a href="#"><img src="../../public/img/menu/facebook.svg?v=<?php echo time() ?>" alt="" srcset=""></a>
+          </div>
+        </div>
+      </div>
+    </div>
     <h2>Booking Reports</h2>
     <div class="booking-reports">
       <!-- Sample Booking Report (replace with dynamic content) -->
